8678c18ae85d2e5a039a709d3a17fc2eddcd41ea
Alex Skrenchuk <alexskr@gmail.com>
2013-07-16 01:49:00 +0000

    Added read from file to buffer functionallity

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer (256);
+
+
+fs.readFile('index.html', 'utf8', function (err, buffer) {
+  if (err) throw err;
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ response.send( buffer.toString('utf8') );
 });
 
 var port = process.env.PORT || 5000;
